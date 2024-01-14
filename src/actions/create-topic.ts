b3d85f190af2e93848fc9325432187d8cfe350b1
Bijay Tamang <tmg.bijay11@gmail.com>
2024-01-14 14:48:12 -0500

    handle form errors

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { z } from 'zod';
+import { auth } from '@/auth';
 
 const createTopicSchema = z.object({
   name: z
@@ -16,6 +17,7 @@ interface CreateTopicFormState {
   errors: {
     name?: string[];
     description?: string[];
+    _form?: string[];
   };
 }
 export async function createTopic(
@@ -33,6 +35,15 @@ export async function createTopic(
     };
   }
 
+  const session = await auth();
+
+  if (!session || !session?.user) {
+    return {
+      errors: {
+        _form: ['You must be signed in to do this.'],
+      },
+    };
+  }
   // TODO: revalidate the homepage
 
   return {
